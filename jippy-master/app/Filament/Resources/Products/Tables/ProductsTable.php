008ac8ab5ff935ff986da771303e12d4f6e2be29
rayhanarzet <reyyyhnz@gmail.com>
2025-11-30 22:17:22 +0700

    feat: add jippy-master folder

---
+++ jippy-master/app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Resources\Products\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+
+class ProductsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable()
+                    ->icon('heroicon-o-shopping-cart')
+                    ->weight('medium'),
+                TextColumn::make('category.name')
+                    ->label('Category')
+                    ->searchable()
+                    ->sortable()
+                    ->badge()
+                    ->color('info'),
+                TextColumn::make('price')
+                    ->label('Price (IDR)')
+                    ->money('IDR', 0)
+                    ->sortable()
+                    ->alignRight(),
+                TextColumn::make('created_at')
+                    ->label('Created')
+                    ->dateTime('d M Y')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: false),
+                TextColumn::make('updated_at')
+                    ->dateTime('d M Y')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                SelectFilter::make('category')
+                    ->relationship('category', 'name')
+                    ->label('Filter by Category')
+                    ->preload(),
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
