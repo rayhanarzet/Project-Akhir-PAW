44fb0e9c83795775adbce7e3c22d0fa1aca5b055
rayhanarzet <reyyyhnz@gmail.com>
2025-12-06 00:40:49 +0700

    Delete jippy-master directory

--- jippy-master/public/menu.js
+++
@@ -1,9 +0,0 @@
- function toggleSidebar() {
-    document.getElementById("sidebar").classList.toggle("open");
-  }
-
-  function toggleDropdown(id) {
-    const submenu = document.getElementById(id);
-    submenu.style.display = submenu.style.display === "block" ? "none" : "block";
-  }
-
