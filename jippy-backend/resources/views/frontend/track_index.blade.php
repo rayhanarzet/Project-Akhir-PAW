ccc97be83b6177a8a5e389055b74ff4cdb4d0fcb
anindya1414 <anindyasafa1406@gmail.com>
2025-12-07 11:50:06 +0700

    Update track_index.blade.php

--- jippy-backend/resources/views/frontend/track_index.blade.php
+++ jippy-backend/resources/views/frontend/track_index.blade.php
@@ -331,7 +331,7 @@
     </div>
 
     <style>
-    /* Footer */
+ 
     footer {
       background-color: #FF96B5;
       padding: 2 rem 1 rem;
@@ -381,7 +381,6 @@
     }
   </style>
 
-  <!-- ===== FOOTER ===== -->
   <footer class="bg-[#FF96B5] py-4">
     <div class="max-w-6xl container mx-auto text-sm text-white">
       <div class="flex justify-between items-center">
