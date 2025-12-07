3749cdb2241488a2f3f81cad8235e542dfd1535a
anindya1414 <anindyasafa1406@gmail.com>
2025-12-07 11:52:17 +0700

    Update track_shipped.blade.php

--- jippy-backend/resources/views/frontend/track_shipped.blade.php
+++ jippy-backend/resources/views/frontend/track_shipped.blade.php
@@ -369,7 +369,6 @@
         
 
     <style>
-    /* Footer */
     footer {
       background-color: #FF96B5;
       padding: 2 rem 1 rem;
@@ -419,7 +418,6 @@
     }
   </style>
 
-  <!-- ===== FOOTER ===== -->
   <footer class="bg-[#FF96B5] py-4">
     <div class="max-w-6xl container mx-auto text-sm text-white">
       <div class="flex justify-between items-center">
